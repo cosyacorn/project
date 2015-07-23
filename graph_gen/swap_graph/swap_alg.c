4ce30f2c5183147ccf84da8db7658e6a9e112f3c
Shane Harding <sharding@tcd.ie>
2015-07-23 12:58:01 +0100

    swap alg working! :)

--- graph_gen/swap_graph/swap_alg.c
+++ graph_gen/swap_graph/swap_alg.c
@@ -101,7 +101,7 @@ int main(int argc, char *argv[]){
 	printf("Set B:\n");
 	for(i=0;i<num_nodes;i++) printf("%d: %d %d %d\n", i, b[i][0], b[i][1], b[i][2]);
 
-	//printf("Time to execute: %f\n", time_taken);
+	printf("Time to execute: %f\n", time_taken);
 
 	// clean up time
 	for(i=0;i<num_nodes;i++){
@@ -145,19 +145,16 @@ void swap_alg(int num_nodes, int num_swaps, int **a, int **b){
 			b_point1 = a[a_first][index_n1]; // point1 in b
 			b_point2 = a[a_first][index_n2]; // point2 in b
 
-			printf("point in A picked:%d\npoints in B neighbouring: %d %d\n", a_first, b_point1, b_point2);
-			//printf("b1 %d, a_first %d, index_n1 %d\n", b_point1, a_first, index_n1);
+
 			index1=0;
 			index2=0;
 
-			//printf("point b %d %d %d a %d\n", b[b_point1][0], b[b_point1][1], b[b_point1][2], a_first);
-
+			// find correct indices
 			while(b[b_point1][index1] != a_first)
 				index1++;
+
 			while(b[b_point2][index2] != a_first)
 				index2++;
-
-			//printf("%d %d\n", index1, index2);
 
 			do{
 				flag=0;
@@ -182,42 +179,36 @@ void swap_alg(int num_nodes, int num_swaps, int **a, int **b){
 				if(b[b_point2][b_index2] == b[b_point1][3-(index1+b_index1)])
 					flag=1;
 
-				if(count>20)
+				// ensure that it stops and starts again if no valid neighbour is found
+				if(count>20){
 					flag2=1;
+					break;
+				}
 
 			} while(flag==1);
-		//	printf("hello\n");
 		} while (flag2==1);
 
+		// set the final points chosen in a
 		a_point1 = b[b_point1][b_index1];
 		a_point2 = b[b_point2][b_index2];
 
-		printf("points in A: %d %d\n", a_point1, a_point2);
 
 		index3=0;
 		index4=0;
 
-		//printf("this should be zero: %d %d\n", index3, index4);
-
-		//printf("a %d %d %d b %d, index3 %d\n",	a[a_point1][0], a[a_point1][1], a[a_point1][2], b[b_point1][b_index1], index3);
-
-		while(a[a_point1][index3] != b_point1){
-			//printf("a %d b %d, index3 %d\n",	a[a_point1][index3], b[b_point1][b_index1], index3);
+		// find correct indices
+		while(a[a_point1][index3] != b_point1)
 			index3++;
-		}
-printf("a %d b %d, index3 %d\n",	a[a_point1][index3], b_point1, index3);
+
 		while(a[a_point2][index4] != b_point2)
 			index4++;
 
-	//printf("%d %d\n", index3, index4);
-
+		// swap the points
 		a[a_point1][index3] = b_point2;
 		a[a_point2][index4] = b_point1;
 
 		b[b_point2][b_index2] = a_point1;
 		b[b_point1][b_index1] = a_point2;
-
-		//printf("points in a: %d %d\n", a_point1, a_point2);
 
 	}
 }
